e4efae3684a5fbb01d6047bcedc24e7d63d7e467
AlvaroVazRod <alvarovazrod@gmail.com>
2021-07-06 12:25:09 +0200

    ClaseInteger1

---
+++ ClaseInteger1/src/ClaseInteger1.java
@@ -0,0 +1,21 @@
+public class ClaseInteger1{
+	@SuppressWarnings("deprecation")
+	public static void main(String args[]){
+	//Creacion de un objeto Integer usando el segundo contructor
+	Integer num1=new Integer("125");
+	
+	//Creacion de un objeto Integer usando el primer contructor
+	Integer num2=new Integer(20);
+	//Obtencion del entero que almacena cada objeto Integer
+	//Si no se hace esto, las lineas siguientes causarian error
+	// de compilacion
+	int n1=num1.intValue();
+	int n2=num2.intValue();
+	System.out.println("Syna de "+n1+" y "+n2+" vale "+(n1+n2));
+	if(n1+n2>130)
+		n1++;
+	else
+		n1--;
+	System.out.println(n1);
+	}
+}
